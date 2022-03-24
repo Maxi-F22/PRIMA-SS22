6e118c06006176b689407c9f0c545ed07cb4265f
Maximilian Flack <maximilian.flack@outlook.de>
2022-03-24 19:33:57 +0100

    add(pacman): Key events

--- Projects/Pacman/Script/Build/Script.js
+++ Projects/Pacman/Script/Build/Script.js
@@ -42,22 +42,64 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     let pacman;
-    let speed = new ƒ.Vector3(1 / 60, 0, 0);
+    let borders;
+    let move_direction = "";
+    let direction_change = "";
+    let speed = 1 / 100;
+    let speed_direction = {
+        up: new ƒ.Vector3(0, speed, 0),
+        down: new ƒ.Vector3(0, -speed, 0),
+        right: new ƒ.Vector3(speed, 0, 0),
+        left: new ƒ.Vector3(-speed, 0, 0),
+    };
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
         let graph = viewport.getBranch();
         pacman = graph.getChildrenByName("Pacman")[0];
-        console.log(pacman);
+        borders = graph.getChildrenByName("Grid")[0].getChildrenByName("Borders")[0].getChildren();
+        for (let border_types of borders) {
+            for (let border of border_types.getChildren()) {
+                console.log(border.mtxLocal.translation);
+            }
+        }
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
-        ƒ.Keyboard;
-        pacman.mtxLocal.translate(speed);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W])) {
+            move_direction = "up";
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S])) {
+            move_direction = "down";
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D])) {
+            move_direction = "right";
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])) {
+            move_direction = "left";
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
+            move_direction = "";
+            direction_change = "";
+        }
+        move(move_direction);
         viewport.draw();
         ƒ.AudioManager.default.update();
+    }
+    function move(direction) {
+        if (direction !== "") {
+            if (direction !== "" && (direction === "up" || direction === "down") && pacman.mtxLocal.translation.x % 1 < 0.01) {
+                direction_change = direction;
+            }
+            else if (direction !== "" && (direction === "left" || direction === "right") && pacman.mtxLocal.translation.y % 1 < 0.01) {
+                direction_change = direction;
+            }
+        }
+        if (direction_change !== "") {
+            pacman.mtxLocal.translate(speed_direction[direction_change]);
+        }
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Projects/Pacman/Script/Source/Main.ts
+++ Projects/Pacman/Script/Source/Main.ts
@@ -4,25 +4,71 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
   let pacman: ƒ.Node;
-  let speed: ƒ.Vector3 = new ƒ.Vector3(1/60, 0, 0);
+  let borders: ƒ.Node[];
+  let move_direction: string = "";
+  let direction_change: string = "";
+  let speed: number = 1/100;
+  let speed_direction: any = {
+    up: new ƒ.Vector3(0, speed, 0),
+    down: new ƒ.Vector3(0, -speed, 0),
+    right: new ƒ.Vector3(speed, 0, 0),
+    left: new ƒ.Vector3(-speed, 0, 0),
+  };
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
     let graph: ƒ.Node = viewport.getBranch();
-    pacman = graph.getChildrenByName("Pacman")[0];
-    console.log(pacman);
 
+    pacman = graph.getChildrenByName("Pacman")[0];
+    borders = graph.getChildrenByName("Grid")[0].getChildrenByName("Borders")[0].getChildren();
+    for (let border_types of borders) {
+      for (let border of border_types.getChildren()) {
+        console.log(border.mtxLocal.translation);
+      }
+    }
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
-    ƒ.Keyboard;
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W])) {
+      move_direction = "up";
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S])) {
+      move_direction = "down";
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D])) {
+      move_direction = "right";
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])) {
+      move_direction = "left";
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
+      move_direction = "";
+      direction_change = "";
+    }
 
-    pacman.mtxLocal.translate(speed);
+    
+
+    move(move_direction);
     viewport.draw();
     ƒ.AudioManager.default.update();
+  }
+
+  function move(direction: string): void {
+    if (direction !== "") {
+      if (direction !== "" && (direction === "up" || direction === "down") && pacman.mtxLocal.translation.x % 1 < 0.01) {
+        direction_change = direction;
+      }
+      else if (direction !== "" && (direction === "left" || direction === "right") && pacman.mtxLocal.translation.y % 1 < 0.01) {
+        direction_change = direction;
+      }
+    }
+    if (direction_change !== "") {
+      pacman.mtxLocal.translate(speed_direction[direction_change]);
+    }
   }
 }

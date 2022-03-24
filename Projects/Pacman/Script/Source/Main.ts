06458c82089a3b74fd4c5bdffc1e7c42c04eaaeb
Maximilian Flack <maximilian.flack@outlook.de>
2022-03-24 10:32:02 +0100

    add (pacman): model  for pacman

--- Projects/Pacman/Script/Source/Main.ts
+++ Projects/Pacman/Script/Source/Main.ts
@@ -3,17 +3,25 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let pacman: ƒ.Node;
+  let speed: ƒ.Vector3 = new ƒ.Vector3(1/60, 0, 0);
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    let graph: ƒ.Node = viewport.getBranch();
+    pacman = graph.getChildrenByName("Pacman")[0];
+    console.log(pacman);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    ƒ.Keyboard;
+
+    pacman.mtxLocal.translate(speed);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }

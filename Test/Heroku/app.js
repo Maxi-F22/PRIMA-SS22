5c7b753c25ba6fe6501e412c82e00d835890a256
Maximilian Flack <maximilian.flack@outlook.de>
2022-05-19 12:02:07 +0200

    fix: heroku error

--- Test/Heroku/app.js
+++ Test/Heroku/app.js
@@ -1,3 +1,5 @@
-"use strict";
-console.log("test");
-//# sourceMappingURL=app.js.map
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, err => {
+  if(err) throw err;
+  console.log("%c Server running", "color: green");
+});
--- Test/Heroku/app.ts
+++ Test/Heroku/app.ts
@@ -1,1 +1,5 @@
-console.log("test");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, err => {
+  if(err) throw err;
+  console.log("%c Server running", "color: green");
+});

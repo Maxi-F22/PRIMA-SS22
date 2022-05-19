fdde11bed7a9218ee159e59e4111e9fd4e1274a4
Maximilian Flack <maximilian.flack@outlook.de>
2022-05-19 12:20:43 +0200

    *

--- Test/Heroku/app.js
+++ Test/Heroku/app.js
@@ -1,27 +1,19 @@
-System.register(["http"], function (exports_1, context_1) {
-    "use strict";
-    var Http, port;
-    var __moduleName = context_1 && context_1.id;
-    function startServer(_port) {
-        let server = Http.createServer();
-        server.addListener("listening", handleListen);
-    }
-    function handleListen() {
-        console.log("Listening");
-    }
-    return {
-        setters: [
-            function (Http_1) {
-                Http = Http_1;
-            }
-        ],
-        execute: function () {
-            port = Number(process.env.PORT);
-            if (!port) {
-                port = 8100;
-            }
-            startServer(port);
-        }
-    };
-});
-//# sourceMappingURL=app.js.map
+import * as Http from "http";
+
+
+let port = Number(process.env.PORT);
+
+if (!port) {
+    port = 8100;
+}
+
+startServer(port);
+
+function startServer(_port) {
+    let server = Http.createServer();
+    server.addListener("listening", handleListen);
+}
+
+function handleListen() {
+    console.log("Listening");
+}
